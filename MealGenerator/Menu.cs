e35a5b92509fe90cadec59113ce032fcc9327ef2
tsaniagb <tsaniagaluh@gmail.com>
2023-12-22 08:49:39 +0700

    word case change

--- MealGenerator/Menu.cs
+++ MealGenerator/Menu.cs
@@ -25,15 +25,15 @@ namespace MealPlanGenerator
         {
             if (bmi < 18.5)
             {
-                return "underweight";
+                return "Underweight";
             }
             else if (bmi < 25)
             {
-                return "normal";
+                return "Normal";
             }
             else
             {
-                return "overweight";
+                return "Overweight";
             }
         }
 
@@ -42,11 +42,11 @@ namespace MealPlanGenerator
             // Generate breakfast menu based on BMI category
             switch (bmiCategory)
             {
-                case "underweight":
+                case "Underweight":
                     return "Variation 1: Whole grain toast with avocado\nVariation 2: Greek yogurt with fruits\nVariation 3: Oatmeal with nuts";
-                case "normal":
+                case "Normal":
                     return "Variation 1: Scrambled eggs with vegetables\nVariation 2: Whole grain cereal with milk\nVariation 3: Smoothie with protein";
-                case "overweight":
+                case "Overweight":
                     return "Variation 1: Veggie omelette\nVariation 2: Quinoa bowl with vegetables\nVariation 3: Low-fat cottage cheese with berries";
                 default:
                     return "Invalid BMI category";
@@ -58,11 +58,11 @@ namespace MealPlanGenerator
             // Generate lunch menu based on BMI category
             switch (bmiCategory)
             {
-                case "underweight":
+                case "Underweight":
                     return "Variation 1: Grilled chicken salad\nVariation 2: Quinoa and vegetable stir-fry\nVariation 3: Lentil soup with whole grain bread";
-                case "normal":
+                case "Normal":
                     return "Variation 1: Grilled salmon with sweet potato\nVariation 2: Turkey and avocado wrap\nVariation 3: Chickpea salad with olive oil dressing";
-                case "overweight":
+                case "Overweight":
                     return "Variation 1: Grilled vegetable and quinoa bowl\nVariation 2: Turkey and vegetable stir-fry\nVariation 3: Lentil and vegetable curry";
                 default:
                     return "Invalid BMI category";
@@ -74,11 +74,11 @@ namespace MealPlanGenerator
             // Generate dinner menu based on BMI category
             switch (bmiCategory)
             {
-                case "underweight":
+                case "Underweight":
                     return "Variation 1: Baked salmon with asparagus\nVariation 2: Spinach and feta-stuffed chicken breast\nVariation 3: Brown rice and black bean bowl";
-                case "normal":
+                case "Normal":
                     return "Variation 1: Quinoa-stuffed bell peppers\nVariation 2: Grilled shrimp with quinoa\nVariation 3: Stir-fried tofu with broccoli";
-                case "overweight":
+                case "Overweight":
                     return "Variation 1: Grilled chicken with roasted vegetables\nVariation 2: Zucchini noodles with tomato sauce\nVariation 3: Cauliflower rice with mixed vegetables";
                 default:
                     return "Invalid BMI category";
